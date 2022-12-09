206fb109c9450c1e7467ac953677db80c13160d1
CarolLuz <acarol.luz@gmail.com>
2022-12-09 17:03:34 -0300

    update files

--- README.mk
+++ README.mk
@@ -1,5 +1,30 @@
-# UNDER CONSTRUCTION
+## Introduction
+This project was developed as part of the module Programming Techniques II, from the Diversidade Tech program, promoted by Let's Code from Ada and Suzano, in december/2022.
 
-Module.py is the same as 1_data_treatment.ipynb
-	1_data_treatment.ipynb was kept for better dataviz
-	module.py was created to import the pre-treated data to other files
+Its goals were to perform a brief exploratory analysis on cholera cases, deaths and fatality rate from different continents and World Health Organization (WHO) regions.
+
+## Dataset
+
+The dataset (data.csv) was obtained from Kaggle (https://www.kaggle.com/datasets/imdevskp/cholera-dataset).
+
+## Visualização dos dados
+Analysis is better observed in the .html reports, found in the following directory (...). These files were provided to favor dataviz and remove the need to run the codes. It is also recommended to open the .html and .ipynb files in Mozilla Firefox.
+
+
+## Data treament files
+
+Module.py is the same as 1_data_treatment.ipynb: 1_data_treatment.ipynb was kept for better dataviz, and module.py was created to import the pre-treated data to the other files.
+
+## Technologies
+
+ - [ ] Python
+ - [ ] Jupyter Notebook
+ - [ ] Git e Github
+
+## Python libraries
+
+ - [ ] [pandas](https://pandas.pydata.org/)
+ - [ ] [numpy](https://numpy.org/)
+ - [ ] [matplotlib](https://matplotlib.org/)
+ - [ ] [seaborn](https://seaborn.pydata.org/)
+ - [ ] [plotly](https://plotly.com/python/)
